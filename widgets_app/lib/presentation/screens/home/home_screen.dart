7bb8441782e15c49729cf02a84d768a371db8f61
Andrés Rentería <andres.renteria@puntored.co>
2023-07-17 15:44:24 -0500

    add list tile to home screen

--- widgets_app/lib/presentation/screens/home/home_screen.dart
+++ widgets_app/lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/menu/menu_item.dart';
+import 'package:widgets_app/presentation/widgets/texts/custom_list_tile.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -7,8 +9,28 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter + Material3'),
+        title: const Text('Flutter + Material3'),
       ),
+      body: HomeView(),
+    );
+  }
+}
+
+class HomeView extends StatelessWidget {
+  const HomeView({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    const menuItems = AppMenuItems;
+
+    return ListView.builder(
+      physics: const BouncingScrollPhysics(),
+      itemCount: menuItems.length,
+      itemBuilder: (context, index) {
+        return CustomListTile(menuItem: menuItems[index]);
+      },
     );
   }
 }
---
+++ widgets_app/lib/presentation/widgets/texts/custom_list_tile.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:widgets_app/config/menu/menu_item.dart';
+
+class CustomListTile extends StatelessWidget {
+  const CustomListTile({
+    super.key,
+    required this.menuItem,
+  });
+
+  final MenuItem menuItem;
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return Column(
+      children: [
+        ListTile(
+          leading: Icon(menuItem.icon, color: colors.primary),
+          onTap: () {},
+          subtitle: Text(menuItem.subtitle),
+          title: Text(menuItem.title),
+          trailing: Icon(Icons.arrow_forward_ios_outlined, color: colors.primary),
+        ),
+        const Divider(
+          endIndent: 10,
+          indent: 10,
+          thickness: 2,
+        )
+      ],
+    );
+  }
+}
