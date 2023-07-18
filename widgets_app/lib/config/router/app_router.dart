e243777c8d9221a924dc776b8fe0eba47326b741
Andrés Rentería <andres.renteria@puntored.co>
2023-07-18 12:49:37 -0500

    add new screens

--- widgets_app/lib/config/router/app_router.dart
+++ widgets_app/lib/config/router/app_router.dart
@@ -5,4 +5,11 @@ final appRouter = GoRouter(initialLocation: '/', routes: [
   GoRoute(path: '/', builder: (context, state) => const HomeScreen()),
   GoRoute(path: '/buttons', builder: (context, state) => const ButtonsScreen()),
   GoRoute(path: '/cards', builder: (context, state) => const CardsScreen()),
+  GoRoute(path: '/animated', builder: (context, state) => const AnimatedScreen()),
+  GoRoute(path: '/app-tutorial', builder: (context, state) => const AppTutorialScreen()),
+  GoRoute(path: '/cards', builder: (context, state) => const CardsScreen()),
+  GoRoute(path: '/infinite', builder: (context, state) => const InfiniteScrollScreen()),
+  GoRoute(path: '/progress', builder: (context, state) => const ProgressScreen()),
+  GoRoute(path: '/snackbar', builder: (context, state) => const SnackbarScreen()),
+  GoRoute(path: '/ui-control', builder: (context, state) => const UIControlScreen()),
 ]);
