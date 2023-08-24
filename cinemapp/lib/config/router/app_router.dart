078843c697ed4e11ff866692adaace9087e3dd4a
Renvel <andres.renteria@puntored.co>
2023-08-24 16:41:52 -0500

    modify router

--- cinemapp/lib/config/router/app_router.dart
+++ cinemapp/lib/config/router/app_router.dart
@@ -3,21 +3,22 @@ import 'package:go_router/go_router.dart';
 
 final appRouter = GoRouter(initialLocation: '/', routes: [
   GoRoute(
-    path: '/',
-    name: HomeScreen.path,
-    builder: (context, state) => const HomeScreen(),
-  ),
+      path: '/',
+      name: HomeScreen.path,
+      builder: (context, state) => const HomeScreen(),
+      routes: [
+        GoRoute(
+          path: 'movie/:id',
+          name: MovieScreen.path,
+          builder: (context, state) {
+            final movieID = state.pathParameters['id'] ?? 'no-id';
+            return MovieScreen(movieID: movieID);
+          },
+        ),
+      ]),
   GoRoute(
     path: '/theme-selector',
     name: ThemeSelectorScreen.path,
     builder: (context, state) => const ThemeSelectorScreen(),
-  ),
-  GoRoute(
-    path: '/movie/:id',
-    name: MovieScreen.path,
-    builder: (context, state) {
-      final movieID = state.pathParameters['id'] ?? 'no-id';
-      return MovieScreen(movieID: movieID);
-    },
   ),
 ]);
