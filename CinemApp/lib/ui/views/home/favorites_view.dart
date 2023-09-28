2905fa0b3e4c6b3a2c9c6b6d8599b4e724b4ab83
Andres Rentreria <andres.renteria@puntored.co>
2023-09-28 10:04:40 -0500

    add view when there are no favorites

--- CinemApp/lib/ui/views/home/favorites_view.dart
+++ CinemApp/lib/ui/views/home/favorites_view.dart
@@ -2,6 +2,7 @@ import 'package:cinemapp/ui/providers/storage/favorites_provider.dart';
 import 'package:cinemapp/ui/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class FavoritesView extends ConsumerStatefulWidget {
   const FavoritesView({super.key});
@@ -31,10 +32,33 @@ class _FavoritesViewState extends ConsumerState<FavoritesView> {
   @override
   Widget build(BuildContext context) {
     final favorites = ref.watch(favoritesMoviesProvider).values.toList();
+    final color = Theme.of(context).colorScheme.primary;
     return Scaffold(
-        body: MoviesMasonryGridState(
-      movies: favorites,
-      loadNextPage: loadNextPage,
-    ));
+        body: favorites.isEmpty
+            ? Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.heart_broken_outlined,
+                      size: 60,
+                      color: color,
+                    ),
+                    Text(
+                      AppLocalizations.of(context)!.ohNo,
+                      style: TextStyle(fontSize: 30, color: color),
+                    ),
+                    Text(
+                      AppLocalizations.of(context)!.noFavorites,
+                      style: const TextStyle(fontSize: 20, color: Colors.black45),
+                    ),
+                  ],
+                ),
+              )
+            : MoviesMasonryGridState(
+                movies: favorites,
+                loadNextPage: loadNextPage,
+              ));
   }
 }
