44cde4fb001bdd63a2d1ae63c06515d71e8cfd44
Andres Rentreria <andres.renteria@puntored.co>
2023-10-03 09:10:06 -0500

    change project name

--- fluttter_bloc_cubits/README.md
+++
@@ -1,3 +0,0 @@
-# fluttter_bloc_cubits
-
-A new Flutter project.
--- fluttter_bloc_cubits/android/app/src/main/kotlin/com/example/fluttter_bloc_cubits/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.fluttter_bloc_cubits
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
